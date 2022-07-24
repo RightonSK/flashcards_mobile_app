ac3044a7b6b2959ee5c35ed9096cb202329f5674
Shota Kinoshita <mashota.88@gmail.com>
2022-07-24 17:35:22 +0900

    hooksの導入。細かい修正。ログインページの作成とfirebase authの導入

---
+++ lib/presentation/login_and_register/login_and_register_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class LoginAndRegisterPage extends ConsumerWidget {
+  const LoginAndRegisterPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            // ログイン　(テキスト)
+
+            // アカウント登録 (テキスト)
+            // email text field
+            // password text field
+          ],
+        ),
+      ),
+    );
+  }
+}
