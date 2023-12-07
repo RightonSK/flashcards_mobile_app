a18234be51e571725ce4c47f6b120128c7c3d1e8
Shota Kinoshita <mashota.88@gmail.com>
2023-12-07 18:59:17 +0900

    チュートリアル機能の削除2

--- lib/presentation/settings/settings_page.dart
+++ lib/presentation/settings/settings_page.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flashcards_mobile_app/presentation/settings/pages_from_settings/delete_account/delete_account_page.dart';
 import 'package:flashcards_mobile_app/presentation/settings/pages_from_settings/email_update/email_update_page.dart';
-import 'package:flashcards_mobile_app/presentation/settings/pages_from_settings/introduction/introductio_page.dart';
 import 'package:flashcards_mobile_app/presentation/settings/pages_from_settings/password_update/password_update_page.dart';
 import 'package:flashcards_mobile_app/presentation/settings/settings_viewmodel.dart';
 import 'package:flashcards_mobile_app/repository/user_provider.dart';
